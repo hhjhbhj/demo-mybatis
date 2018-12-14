4def9208061dc1a2ffbae01618fbd6ae099c4e48
hhjhbhj <hhjhbhj@163.com>
2018-12-14 09:51:38 +0800

    demo-mybatis

--- src/main/java/com/example/demomybaitis/controller/IndexController.java
+++ src/main/java/com/example/demomybaitis/controller/IndexController.java
@@ -1,5 +1,19 @@
 package com.example.demomybaitis.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class IndexController {
+  @RequestMapping("/")
+  public ModelAndView index(){
+    Map map = new HashMap();
+
+    return new ModelAndView("index","test","testvalue");
+  }
 
 }
